b5e7a185687eaeeb1c537e62bacd92ed0eac04d4
Lin <ly1328@nyu.edu>
2022-09-08 15:44:16 -0400

    Finish Fill by Clustering in EDA get generate new dataset

--- utils/func_utils.py
+++ utils/func_utils.py
@@ -8,6 +8,17 @@ from .stats_utils import chi_square_test
 
 
 def cross_tabulation_analysis(dataframe, col1, col2):
+    """
+    To performe cross tab analysis with chi square, p value and heatmap
+    """
     chi_square_test(dataframe[col1], dataframe[col2])
     cross = pd.crosstab(index=dataframe[col1], columns=dataframe[col2], normalize="columns")
     plot_heatmap(cross, fmt=".2%")
+
+
+def fill_unknown(dataframe, cols):
+    df_new = dataframe.copy()
+    for col in cols:
+        name_to_sub = dataframe[col].value_counts()[:1].index.tolist()[0] # get the most frequent one
+        df_new.loc[:, col].replace({"unknown": name_to_sub}, inplace=True)
+    return df_new
