9739ee3c8d7bf603e235c53ab0191d77d3a3ed15
Lin <ly1328@nyu.edu>
2022-09-10 14:12:53 -0400

    Modify EDA, add predictive analysis

--- utils/stats_utils.py
+++ utils/stats_utils.py
@@ -1,9 +1,17 @@
 import numpy as np
 import pandas as pd
 import researchpy as rp
+import statsmodels.api as sm
 
 
 def chi_square_test(col1, col2):
     ctab, chitest = rp.crosstab(col1, col2, margins=False, test="chi-square", expected_freqs=False)
     print(ctab)
     print(chitest)
+
+
+def OLS_model(X, y):
+    X = sm.add_constant(X)
+    est = sm.OLS(y, X)
+    est2 = est.fit()
+    print(est2.summary())
